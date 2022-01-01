ee38f89b27a7a0bd2140d29f153a6b9132f66e1b
LucasConte <lucas.co.pro@gmail.com>
2022-01-01 23:03:17 +0100

    Meilleure echelle planètes

--- src/main/java/Main_progr.java
+++ src/main/java/Main_progr.java
@@ -55,22 +55,18 @@ public class Main_progr {
                 StdDraw.text(5,9.5,"Temps écoulé : " + nbrAnnee + "a " + nbrMois + "m " + tempsEnJours + "j");
 
 
-                Etoile soleil = new Etoile("Soleil",Color.YELLOW,0.2,5,5);
+                Etoile soleil = new Etoile("Soleil",Color.YELLOW,0.3,5,5);
                 soleil.afficheToi();
-
-                Planète terre = new Planète("Terre",Color.GREEN,0.1,1,60);
-                terre.afficheTonOrbite();
-                terre.afficheToi();
 
                 List<Planète> planètes;
                 planètes= Arrays.asList(
-                    new Planète("Mercure",Color.DARK_GRAY,0.0025,0.8,87.96),
-                    new Planète("Vénus",Color.LIGHT_GRAY,0.005,0.5,224.69),
-                    new Planète("Terre",Color.GREEN,0.02,0.6,365.256),
-                    new Planète("Mars",Color.RED,0.0325,0.4,686.97),
-                    new Planète("Jupiter",Color.ORANGE,0.0375,1.4,4332.589),
-                    new Planète("Saturne",Color.YELLOW,0.05,2.5,10759.23),
-                    new Planète("Uranus",Color.CYAN,0.125,3,30685.4),
+                    new Planète("Mercure",Color.DARK_GRAY,0.05,0.5,87.96),
+                    new Planète("Vénus",Color.LIGHT_GRAY,0.11,0.80,224.69),
+                    new Planète("Terre",Color.GREEN,0.11,1.2,365.256),
+                    new Planète("Mars",Color.RED,0.06,1.5,686.97),
+                    new Planète("Jupiter",Color.ORANGE,0.275,1.9,4332.589),
+                    new Planète("Saturne",Color.YELLOW,0.25,2.5,10759.23),
+                    new Planète("Uranus",Color.CYAN,0.2,3.25,30685.4),
                     new Planète("Neptune",Color.BLUE,0.2,4,60216.8)
                     );
 
